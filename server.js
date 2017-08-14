a506d9513def9e9c0d48a31bbbab7f09fbdd408f
Josh Daugherty <jd21.daugherty@gmail.com>
2017-08-13 20:25:57 -0500

    enable cors

--- server.js
+++ server.js
@@ -19,6 +19,8 @@ var router = express.Router();
 // middleware to use for all requests
 router.use(function(req, res, next) {
     // do logging
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     console.log('Middleware is working!.');
     next(); // make sure we go to the next routes and don't stop here
 });
