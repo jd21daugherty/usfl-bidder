dbe90847958cbc70fb3c07f08f3e6768db52f5ba
Josh Daugherty <jd21.daugherty@gmail.com>
2017-11-06 21:55:52 -0600

    move all access control origin from router to app instance

--- server.js
+++ server.js
@@ -19,6 +19,12 @@ var admin = require("firebase-admin");
 
 var serviceAccount = require("./sa.json");
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
   databaseURL: "https://usfl-dynasty-values.firebaseio.com"
@@ -30,8 +36,6 @@ var db = admin.firestore();
 // middleware to use for all requests
 router.use(function(req, res, next) {
     // do logging    
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     //console.log('Middleware is working!.');
     next(); // make sure we go to the next routes and don't stop here
 });
