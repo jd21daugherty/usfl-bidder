03035042207b2516ddcafdfff9c7c2bb9f7ec00d
Josh Daugherty <jd21.daugherty@gmail.com>
2018-05-14 21:23:53 -0500

    edited scheduled task to only run tuesday nights

--- scheduled-tasks/save-player-values.js
+++ scheduled-tasks/save-player-values.js
@@ -3,38 +3,43 @@ const request = require("request");
 const saveQbValuesUrl = "https://usfl-node-bidder.herokuapp.com/api/playervalues/qb";
 const saveRbValuesUrl = "https://usfl-node-bidder.herokuapp.com/api/playervalues/rb";
 const saveWrValuesUrl = "https://usfl-node-bidder.herokuapp.com/api/playervalues/wr";
-const saveTeValuesUrl = "https://usfl-node-bidder.herokuapp.com/api/playervalues/te"
+const saveTeValuesUrl = "https://usfl-node-bidder.herokuapp.com/api/playervalues/te";
 
-// save qb values
+var today = new Date();
+
+if(today.getDay() === 2){
+    // save qb values
 request.post(saveQbValuesUrl, (error, response, body) => {
-  let json = JSON.parse(body);
-  console.log("error: " + error);
-  console.log("response: " + response);
-  console.log("body: " + body);
-});
-
-// save rb values
-request.post(saveRbValuesUrl, (error, response, body) => {
     let json = JSON.parse(body);
     console.log("error: " + error);
     console.log("response: " + response);
     console.log("body: " + body);
   });
+  
+  // save rb values
+  request.post(saveRbValuesUrl, (error, response, body) => {
+      let json = JSON.parse(body);
+      console.log("error: " + error);
+      console.log("response: " + response);
+      console.log("body: " + body);
+    });
+  
+  // save wr values
+  request.post(saveWrValuesUrl, (error, response, body) => {
+      let json = JSON.parse(body);
+      console.log("error: " + error);
+      console.log("response: " + response);
+      console.log("body: " + body);
+    });
+  
+  // save te values
+  request.post(saveTeValuesUrl, (error, response, body) => {
+      let json = JSON.parse(body);
+      console.log("error: " + error);
+      console.log("response: " + response);
+      console.log("body: " + body);
+    });  
+}
 
-// save wr values
-request.post(saveWrValuesUrl, (error, response, body) => {
-    let json = JSON.parse(body);
-    console.log("error: " + error);
-    console.log("response: " + response);
-    console.log("body: " + body);
-  });
-
-// save te values
-request.post(saveTeValuesUrl, (error, response, body) => {
-    let json = JSON.parse(body);
-    console.log("error: " + error);
-    console.log("response: " + response);
-    console.log("body: " + body);
-  });
 
 
